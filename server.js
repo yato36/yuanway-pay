8478e0a2b016c2f2727820850dd43e3ede808906
yato36 <114881905+yato36@users.noreply.github.com>
2026-06-03 09:04:50 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.post('/api/get-iframe-token', async (req, res) => {
     try {
         // ✅ الـ endpoint الصحيح حسب LianLian Support
         // التعديل هنا: استخدام /token بدلاً من /payments/elements
-         const url = `https://celer-api.LianLianpay-inc.com/v3/merchants/<merchant_id>/token`;
+         const url = `https://celer-api.LianLianpay-inc.com/v3/merchants/${MERCHANT_ID}/payments/elements`;
         const response = await fetch(url, {
             method:  'POST',
             headers: llHeaders(bodyStr),
